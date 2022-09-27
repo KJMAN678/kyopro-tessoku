f499e6a2252cddf390dd5258291824a020683a67
KJMAN678 <kj.fireman5@gmail.com>
2022-09-27 21:51:47 +0900

    chap01 a02 rustで作成

---
+++ codes/rust/chap01/target/debug/answer_a02.d
@@ -0,0 +1,1 @@
+/Users/shimizukouji/data/kyopro-tessoku/codes/rust/chap01/target/debug/answer_a02: /Users/shimizukouji/data/kyopro-tessoku/codes/rust/chap01/src/answer_a02.rs
---
+++ codes/rust/chap01/target/debug/deps/answer_a02-49dd42abfabea26e.d
@@ -0,0 +1,5 @@
+/Users/shimizukouji/data/kyopro-tessoku/codes/rust/chap01/target/debug/deps/answer_a02-49dd42abfabea26e: src/answer_a02.rs
+
+/Users/shimizukouji/data/kyopro-tessoku/codes/rust/chap01/target/debug/deps/answer_a02-49dd42abfabea26e.d: src/answer_a02.rs
+
+src/answer_a02.rs:
